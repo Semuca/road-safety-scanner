d6c997ba25e5d0dfaf4374c20acf727d4ef60cd8
Semuca <pokelock@icloud.com>
2024-08-22 10:06:10 +0800

    feat(journal): set up basic journal scaffold

--- modules/journal_downloader/downloader.py
+++ modules/journal_downloader/downloader.py
@@ -1,10 +1,34 @@
+import concurrent.futures
+from urllib import request
 from typing import Any
 
+elsevierAPI = "https://api.elsevier.com/content"
+
+def downloadJournal(doi: str) -> Any:
+    """
+    Downloads a journal from the internet
+    """
+    
+    r = request.Request(f"${elsevierAPI}/article/doi/${doi}", headers={'Accept': 'application/json', 'X-ELS-APIKey': ''})
+    journal = request.urlopen(r).read().decode('utf-8')
+
+    return journal
 
 def downloadJournals(searchTerm: str) -> Any:
     """
     Downloads journals from the internet. Should store previous queries run to avoid repetition.
     """
+
+    r = request.Request(f"${elsevierAPI}/search/scopus?q={searchTerm}", headers={'Accept': 'application/json', 'X-ELS-APIKey': ''})
+
+    # Read all journals from the query
+    searchedJournals = request.urlopen(r).read().decode('utf-8')
+
+    # Fetch the returned journal dois
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(downloadJournal, query) for query in queries]
+        results = [future.result() for future in concurrent.futures.as_completed(futures)]
+
     return getJournals()
 
 def getJournals(searchTerm: str) -> Any:
