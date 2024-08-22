07926c8286f222fe2f12342ebc9480eb5381d39c
Semuca <pokelock@icloud.com>
2024-08-22 11:22:34 +0800

    feat(journal): add basic journal downloading from scopus query

--- modules/journal_downloader/downloader.py
+++ modules/journal_downloader/downloader.py
@@ -1,38 +1,45 @@
 import concurrent.futures
+import json
+import os
 from urllib import request
 from typing import Any
 
 elsevierAPI = "https://api.elsevier.com/content"
+ELSEVIER_API_KEY = os.environ.get('ELSEVIER_API_KEY')
 
 def downloadJournal(doi: str) -> Any:
     """
-    Downloads a journal from the internet
+    Downloads a journal from the Elsevier API.
     """
     
-    r = request.Request(f"${elsevierAPI}/article/doi/${doi}", headers={'Accept': 'application/json', 'X-ELS-APIKey': ''})
+    r = request.Request(f"{elsevierAPI}/article/doi/${doi}", headers={'Accept': 'application/json', 'X-ELS-APIKey': ELSEVIER_API_KEY})
     journal = request.urlopen(r).read().decode('utf-8')
 
-    return journal
+    with open(f"journals/{doi.replace('/', '-')}.json", "w") as f:
+        f.write(journal)
 
-def downloadJournals(searchTerm: str) -> Any:
+    return json.loads(journal)
+
+def downloadJournals(searchTerm: str) -> list[Any]:
     """
     Downloads journals from the internet. Should store previous queries run to avoid repetition.
     """
 
-    r = request.Request(f"${elsevierAPI}/search/scopus?q={searchTerm}", headers={'Accept': 'application/json', 'X-ELS-APIKey': ''})
+    r = request.Request(f"{elsevierAPI}/search/scopus?query={searchTerm}", headers={'Accept': 'application/json', 'X-ELS-APIKey': ELSEVIER_API_KEY})
 
     # Read all journals from the query
-    searchedJournals = request.urlopen(r).read().decode('utf-8')
+    searchedJournals = json.loads(request.urlopen(r).read().decode('utf-8'))
+    dois = [query["prism:doi"] for query in searchedJournals["search-results"]["entry"]]
 
     # Fetch the returned journal dois
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [executor.submit(downloadJournal, query) for query in queries]
-        results = [future.result() for future in concurrent.futures.as_completed(futures)]
+    executor = concurrent.futures.ThreadPoolExecutor()
+    results = list(executor.map(downloadJournal, dois))
+    executor.shutdown()
 
-    return getJournals()
+    return results
 
 def getJournals(searchTerm: str) -> Any:
     """
-    Returns the journals in the journals directory. May parse the journals.
+    Returns the journals in the journals directory.
     """
     pass
