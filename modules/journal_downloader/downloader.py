b3082431ad19351df9bbc83072b17cfeefc09de7
Semuca <pokelock@icloud.com>
2024-08-22 13:05:53 +0800

    feat(journal): add limits and wait times for journal requests

--- modules/journal_downloader/downloader.py
+++ modules/journal_downloader/downloader.py
@@ -1,8 +1,10 @@
 import concurrent.futures
 import json
 import os
-from urllib import request
+import time
 from typing import Any
+import urllib.error
+import urllib.request
 
 elsevierAPI = "https://api.elsevier.com/content"
 ELSEVIER_API_KEY = os.environ.get('ELSEVIER_API_KEY')
@@ -12,34 +14,46 @@ def downloadJournal(doi: str) -> Any:
     Downloads a journal from the Elsevier API.
     """
     
-    r = request.Request(f"{elsevierAPI}/article/doi/${doi}", headers={'Accept': 'application/json', 'X-ELS-APIKey': ELSEVIER_API_KEY})
-    journal = request.urlopen(r).read().decode('utf-8')
+    r = urllib.request.Request(f"{elsevierAPI}/article/doi/${doi}", headers={'Accept': 'application/json', 'X-ELS-APIKey': ELSEVIER_API_KEY})
+    try:
+        journal = urllib.request.urlopen(r).read().decode('utf-8')
+    except urllib.error.HTTPError as e:
+        print(e.read().decode())
+        return
 
     with open(f"journals/{doi.replace('/', '-')}.json", "w") as f:
         f.write(journal)
 
     return json.loads(journal)
 
-def downloadJournals(searchTerm: str) -> list[Any]:
+def downloadJournals(query: str, limit = 70, wait=1) -> list[Any]:
     """
     Downloads journals from the internet. Should store previous queries run to avoid repetition.
     """
 
-    r = request.Request(f"{elsevierAPI}/search/scopus?query={searchTerm}", headers={'Accept': 'application/json', 'X-ELS-APIKey': ELSEVIER_API_KEY})
+    r = urllib.request.Request(f"{elsevierAPI}/search/scopus?query={query}", headers={'Accept': 'application/json', 'X-ELS-APIKey': ELSEVIER_API_KEY})
 
-    # Read all journals from the query
-    searchedJournals = json.loads(request.urlopen(r).read().decode('utf-8'))
-    dois = [query["prism:doi"] for query in searchedJournals["search-results"]["entry"]]
+    # Read all journals from the query until limit is hit
+    dois = []
+    totalResults = 1
+    while (len(dois) < limit and len(dois) < totalResults):
+        r = urllib.request.Request(f"{elsevierAPI}/search/scopus?query={query}&start={len(dois)}", headers={'Accept': 'application/json', 'X-ELS-APIKey': ELSEVIER_API_KEY})
+        
+        searchedJournals = json.loads(urllib.request.urlopen(r).read().decode('utf-8'))
+        totalResults = int(searchedJournals["search-results"]["opensearch:totalResults"])
 
-    # Fetch the returned journal dois
-    executor = concurrent.futures.ThreadPoolExecutor()
-    results = list(executor.map(downloadJournal, dois))
-    executor.shutdown()
+        dois.extend([query["prism:doi"] for query in searchedJournals["search-results"]["entry"]])
+        time.sleep(wait)
+
+    results = []
+    for doi in dois:
+        results.append(downloadJournal(doi))
+        time.sleep(wait)
 
     return results
 
-def getJournals(searchTerm: str) -> Any:
-    """
-    Returns the journals in the journals directory.
-    """
-    pass
+# def getJournals() -> Any:
+#     """
+#     Returns the journals in the journals directory.
+#     """
+#     pass
